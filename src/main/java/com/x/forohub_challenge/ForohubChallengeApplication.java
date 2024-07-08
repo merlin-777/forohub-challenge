2ce83c659de3afc756a9982c43237d47a0c6069a
merlin-777 <amerlinrv1@gmail.com>
2024-07-08 16:06:58 -0500

    Spring Boot project initialized

---
+++ src/main/java/com/x/forohub_challenge/ForohubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.x.forohub_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/x/forohub_challenge/ForohubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x.forohub_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
